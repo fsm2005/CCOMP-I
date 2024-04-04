6310335110cca1e82d7c3f7255133d45de4c2705
fsm2005 <143094862+fsm2005@users.noreply.github.com>
2024-04-04 07:46:36 -0500

    Add files via upload

---
+++ Dias.h
@@ -0,0 +1,55 @@
+#include <iostream>
+class Dias {
+public:
+    Dias(int mes, int dia, int año) : m(mes), d(dia), a(año) {
+        if (mes >= 12 || mes < 1) {
+            m = 1;
+        }
+        if (año <= 1900 || año >= 2050) {
+            a = 1900;
+        }
+    }
+
+    bool isleapYear() const {
+        if (a % 4 == 0 && a % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void setMes(int mes) {
+        if (mes >= 12 || mes < 1) {
+            m = mes;
+        }
+    }
+
+    void setDia(int dia) {
+        d = dia;
+    }
+
+    void setAño(int año) {
+        a = año;
+    }
+
+    int getMes() const {
+        return m;
+    }
+
+    int getDia() const {
+        return d;
+    }
+
+    int getAño() const {
+        return a;
+    }
+
+    void displayDate() const {
+        std::cout << m << "/" << d << "/" << a << std::endl;
+    }
+
+private:
+    int m;
+    int d;
+    int a;
+};
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Dias.h"
+int main() {
+    Dias fecha1(3, 1, 2020);
+    Dias fecha2(15, 14, 2019);
+    Dias fecha3(20, 12, 1500);
+
+    std::cout << "Fecha 1: ";
+    fecha1.displayDate();
+    fecha1.isleapYear();
+
+    std::cout << "Fecha 2: ";
+    fecha2.displayDate();
+    fecha1.isleapYear();
+
+    std::cout << "Fecha 3: ";
+    fecha3.displayDate();
+    fecha1.isleapYear();
+
+}
